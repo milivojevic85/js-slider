063d497673ce52c7e212b6fdacd73670568d4a1e
mvicentijevic <mvicentijevic85@gmail.com>
2022-02-02 20:22:26 +0100

    slider responsive

--- src/app/core/core.js
+++ src/app/core/core.js
@@ -1,15 +1,19 @@
 import { render } from '../render/render';
 import { renderSlides } from '../render/render';
+import { calculateSize } from '../render/render';
 import { moveLeft, moveRight } from '../events/events';
 
 export default function initialize() {
   this.render = render;
   this.renderSlides = renderSlides;
+  this.calculateSize = calculateSize;
   this.render();
   this.renderSlides(this.data);
+  this.checkScreenResolution();
   this.moveLeft = moveLeft;
   this.moveRight = moveRight;
   this.rightArrow.addEventListener('click', this.moveRight.bind(this));
   this.leftArrow.addEventListener('click', this.moveLeft.bind(this));
+  window.addEventListener('resize', this.checkScreenResolution.bind(this));
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import './scss/main.scss';
-
-const sayHi = () => console.log('Hi');
-
-import './scss/main.scss';
 import core from './app/core/core';
+import checkScreenResolution from './app/helper/checkScreenResolution';
 
 class Slider {
   constructor(el, data, options) {
     this.el = el;
     this.data = data;
+    this.checkScreenResolution = checkScreenResolution;
     this.initialize = core;
     this.previewType = 6;
     this.countInfinity = 0;
